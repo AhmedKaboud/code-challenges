f3c621e8ab5ee4f33e0d7a3c1f48d8f6cbec9233
akaboud <akaboud@amazon.com>
2021-05-30 14:18:27 +0100

    Solve Connected Graph in Java

--- java_scala/src/main/scala/com/intenthq/challenge/JConnectedGraph.java
+++ java_scala/src/main/scala/com/intenthq/challenge/JConnectedGraph.java
@@ -1,7 +1,11 @@
 package com.intenthq.challenge;
 
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
+import java.util.Set;
 
 public class JConnectedGraph {
   // Find if two nodes in a directed graph are connected.
@@ -15,7 +19,26 @@ public class JConnectedGraph {
   // run(a, c) == true
   // run(b, d) == false
   public static boolean run(JNode source, JNode target) {
-    throw new RuntimeException("Not implemented");
+    if(source == target)
+      return true;
+
+    Queue<JNode> nodes = new LinkedList<>();
+    Set<JNode> visitedNodes = new HashSet<>();
+    nodes.add(source);
+    visitedNodes.add(source);
+    while(!nodes.isEmpty()) {
+      JNode currentNode = nodes.remove();
+      for(JNode childNode : currentNode.edges) {
+        if(childNode == target) {
+          return true;
+        }
+        else if(!visitedNodes.contains(childNode)){
+          nodes.add(childNode);
+          visitedNodes.add(childNode);
+        }
+      }
+    }
+    return false;
   }
 
   public static class JNode {
